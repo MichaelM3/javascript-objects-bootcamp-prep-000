9f0b194dbe626e6274d39f0c69b5572a47c35020
Michael Muniz <michael.muniz94@gmail.com>
2018-09-12 00:35:14 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 var playlist = { 'Slowdive': 'Alison', 'My Bloody Valentine': 'Sometimes' }
 
 function updatePlaylist(playlist, artistName, songTitle) {
@@ -7,3 +8,13 @@ function updatePlaylist(playlist, artistName, songTitle) {
 function removeFromPlaylist(playlist, artistName) {
   return delete playlist.Slowdive
 }
+=======
+var playlist = { linkinPark: 'faint' }
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  return Object.assign({}, obj, { [key]: value })
+}
+
+const playlist = { 'Slowdive': 'Alison', 'My Bloody Valentine': 'Sometimes' }
+updatePlaylist(playlist, 'Phil Ochs', "Here's to the State of Mississippi")
+>>>>>>> 3435267f282e272fa3f2c104d0c6c5bb41f1b91a
